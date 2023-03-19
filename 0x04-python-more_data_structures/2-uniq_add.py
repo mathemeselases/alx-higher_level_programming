33c407b58be4a11f719d72bc48aa2b76b2095ec6
mathemeselases <mathemeselasesk@gmail.com>
2023-03-19 02:40:19 -0700

    addtion

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
-
+    unique_values = []
+    sum = 0
+    for elem in my_list:
+        if elem in unique_values:
+            continue
+        else:
+            unique_values.append(elem)
+    for item in unique_values:
+        sum += item
+    return sum

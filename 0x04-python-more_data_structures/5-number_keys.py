1e7cc69882626bec2fa4732a141a3f6838384d25
mathemeselases <117817977+mathemeselases@users.noreply.github.com>
2023-05-02 02:57:52 -0700

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def number_keys(my_dict):
-    return len(my_dict)
+def number_keys(a_dictionary):
+    count = 0
+    for key, elem in enumerate(a_dictionary):
+        count += 1
+    return count
 
